0e02d4f5f6149979c27a3cc90c4e0cfbdacbce7f
Sinya <grace5334@gmail.com>
2014-06-07 05:39:30 +0800

    response

--- js/script.js
+++ js/script.js
@@ -211,7 +211,7 @@ FB.api('/me/albums?fields=id,name', function(response) {
 
     console.log(album.name);
 
-    $("#album").append('<option id="albumID" value='+ i +album.name+">" + "</option>");
+    $("#album").append('<option id="albumID" value='+ album.name + ">" + "</option>");
 
     // if (album.name == 'Profile Pictures'){
 
