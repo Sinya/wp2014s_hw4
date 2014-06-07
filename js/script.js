66248b58681b6913c684fb72998b10e69eb16666
Sinya <grace5334@gmail.com>
2014-06-07 23:33:01 +0800

    response

--- js/script.js
+++ js/script.js
@@ -284,8 +284,8 @@ function photoClick(id) {
 			// canvas.width = profileIMG.width;//設定canvas的大小需符合profileimg的大小
 			// canvas.height = profileIMG.height;
 
-			canvas.width = 100;//設定canvas的大小需符合profileimg的大小
-			canvas.height = 100;
+			canvas.width = 1000;//設定canvas的大小需符合profileimg的大小
+			canvas.height = 7000;
 			console.log(profileIMG.height)
 			console.log(profileIMG.width)
 
