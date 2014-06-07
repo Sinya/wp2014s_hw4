5cfa77bba770d77135246fdc0b6a8f1f2f500450
Sinya <grace5334@gmail.com>
2014-06-08 03:18:31 +0800

    response

--- js/script.js
+++ js/script.js
@@ -328,7 +328,7 @@ for (var i = 0; i < response.data.length; i++) {
     var album = response.data[i];
    	  if (album.name == val) {
    	  	console.log(album.name)
-		function getMyPhoto() {
+		function getMyPhoto(album) {
 		// $("#photo img").remove();
 		     FB.api('/'+album.id+'/photos', function(photos){
 		       if (photos && photos.data && photos.data.length){
