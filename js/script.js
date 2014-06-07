58f5bcbf07436c56f2017fcfe5b0aef27c8323a4
Sinya <grace5334@gmail.com>
2014-06-08 05:17:50 +0800

    response

--- js/script.js
+++ js/script.js
@@ -299,7 +299,7 @@ FB.api('/me/albums?fields=id,name', function(response) {
       canMouseX=parseInt(e.clientX-offsetX);
       canMouseY=parseInt(e.clientY-offsetY);
       ctx.clearRect(0,0,canvasWidth,canvasHeight); //移除canvas起始的內容
-	  ctx.drawImage(img2,0,0); //劃入img2
+	  // ctx.drawImage(img2,0,0); //劃入img2
       ctx.drawImage(selectIMG,canMouseX-128/2,canMouseY-120/2); //劃入img3，並根據你的滑鼠游標移動，你可以自行更換想要移動的圖層，數值會因XY軸向有所不同
      }
 
