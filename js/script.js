a0a35a0720913f389d3f42c02f31ed6f11eabc1a
Sinya <grace5334@gmail.com>
2014-06-07 23:17:09 +0800

    response

--- js/script.js
+++ js/script.js
@@ -277,9 +277,9 @@ function photoClick(id) {
     var canvasHeight=canvas.height;//高度
     var isDragging=false;//拖拉
 
-	document.getElementById(id).setAttribute('id', 'preview1');
+	// document.getElementById(id).setAttribute('id', 'preview1');
           	ctx.clearRect(0,0,canvasWidth,canvasHeight); //移除canvas起始的內容
-			var profileIMG = document.getElementById("preview1");//抓html裡預載入的照片
+			var profileIMG = document.getElementById(id);//抓html裡預載入的照片
 			profileIMG.crossOrigin = "Anonymous"; // 這務必要做，為了讓Facebook的照片能夠crossdomain傳入到你的頁面，CORS Policy請參考https://developer.mozilla.org/en-US/docs/Web/HTML/CORS_enabled_image 
 			canvas.width = profileIMG.width;//設定canvas的大小需符合profileimg的大小
 			canvas.height = profileIMG.height;
