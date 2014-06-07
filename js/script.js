b5b1214725d6f37fc6b5cc2850af74b36be90605
Sinya <grace5334@gmail.com>
2014-06-07 21:52:48 +0800

    response

--- js/script.js
+++ js/script.js
@@ -222,7 +222,8 @@ FB.api('/me/albums?fields=id,name', function(response) {
             $("#photo").append("<img id="+photo.id + "src=" + image.src  + ">");
 
             // document.body.appendChild(image);
-          // }
+        }
+          }
         }
       });
 
