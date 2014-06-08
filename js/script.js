f5eeabb8b19ba0d58e199a90ede0e1511349879a
Sinya <grace5334@gmail.com>
2014-06-08 21:42:07 +0800

    response

--- js/script.js
+++ js/script.js
@@ -233,13 +233,9 @@ FB.api('/me/albums?fields=id,name', function(response) {
 // };
 
 
-	var canvas=document.getElementById("layer1"); //宣告變數找到canvas標籤
+	var canvas=document.getElementById("canvas"); //宣告變數找到canvas標籤
     var ctx=canvas.getContext("2d"); //找到2d內容
-    var canvasOffset=$("#layer1").offset();//找到offset
-
-    var canvas2=document.getElementById("layer2"); //宣告變數找到canvas標籤
-    var ctx=canvas2.getContext("2d"); //找到2d內容
-    var canvasOffset=$("#layer2").offset();//找到offset
+    var canvasOffset=$("#canvas").offset();//找到offset
 
     var offsetX=canvasOffset.left;//左方
     var offsetY=canvasOffset.top;//上方
@@ -257,18 +253,13 @@ FB.api('/me/albums?fields=id,name', function(response) {
  //起始畫面
 
 
-	var ctx = document.getElementById('layer1').getContext('2d'); //宣告變數找到頁面的canvas標籤的2d內容
-	var ctx2 = document.getElementById('layer2').getContext('2d'); //宣告變數找到頁面的canvas標籤的2d內容
-
+	var ctx = document.getElementById('canvas').getContext('2d'); //宣告變數找到頁面的canvas標籤的2d內容
 	ctx.font='20px "Arial"'; //設定字體與大小
-	ctx2.font='20px "Arial"'; //設定字體與大小
-
 	ctx.fillText("Click here to start fill with Facebook Profile Picture", 40, 270); //設定預設的開始畫面
-
     var img = new Image(); // 新增圖像1
     img.src = "img/overlay.png"; //圖像路徑（路徑自己設，且自己加入想要的圖層）
 	var img2 = new Image(); //新增圖像2 
-	img2.src = "img/overlay1.png" //圖像路徑
+	img2.src = "img/overlayback.png" //圖像路徑
 	var img3 = new Image();//新增圖像3
 	img3.src = "img/typography.png"//圖像路徑
 
@@ -277,15 +268,15 @@ FB.api('/me/albums?fields=id,name', function(response) {
     var val = $("#pattern option:selected").text();
     console.log(val)
     img2.src = "img/" + val + ".png" ; //圖像路徑
-		// if (selectIMG != undefined) {
-	  	    // ctx2.clearRect(0,0,canvasWidth,canvasHeight); //移除canvas起始的內容
-			ctx2.drawImage(img2,0,0); //劃入img2
-	  		// ctx.drawImage(selectIMG,canMouseX-150,canMouseY); //劃入img3，並根據你的滑鼠游標移動，你可以自行更換想要移動的圖層，數值會因XY軸向有所不同
-	     // }
+		if (selectIMG != undefined) {
+	  	    ctx.clearRect(0,0,canvasWidth,canvasHeight); //移除canvas起始的內容
+			ctx.drawImage(img2,0,0); //劃入img2
+	  		ctx.drawImage(selectIMG,canMouseX-150,canMouseY); //劃入img3，並根據你的滑鼠游標移動，你可以自行更換想要移動的圖層，數值會因XY軸向有所不同
+	     }
 	});
 
 
-	$( "#layer1" ).mouseover(function() {
+	$( "#canvas" ).mouseover(function() {
 
 	function handleMouseDown(e){//滑鼠按下的函數
       canMouseX=parseInt(e.clientX-offsetX);//抓滑鼠游標X
@@ -305,7 +296,7 @@ FB.api('/me/albums?fields=id,name', function(response) {
       canMouseX=parseInt(e.clientX-offsetX);
       canMouseY=parseInt(e.clientY-offsetY);
       // user has left the canvas, so clear the drag flag
-      isDragging=false;
+      //isDragging=false;
     }
 
     function handleMouseMove(e){//滑鼠移動的event
@@ -314,20 +305,20 @@ FB.api('/me/albums?fields=id,name', function(response) {
    // if the drag flag is set, clear the canvas and draw the image
 	      if(isDragging){ //當拖拉為True時
 	  
-			  if (selectIMG != undefined) {
+			  if (selectIMG != undefined)
 			  	    ctx.clearRect(0,0,canvasWidth,canvasHeight); //移除canvas起始的內容
-	  				// ctx2.drawImage(img2,0,0); //劃入img2
-		      		ctx.drawImage(selectIMG,canMouseX,canMouseY); 
-		      	}
+	  				ctx.drawImage(img2,0,0); //劃入img2
+
+		      		ctx.drawImage(selectIMG,canMouseX-150,canMouseY); //劃入img3，並根據你的滑鼠游標移動，你可以自行更換想要移動的圖層，數值會因XY軸向有所不同
 	     }
      }
 
 
 	// 抓取滑鼠移動的event
-    $("#layer1").mousedown(function(e){handleMouseDown(e);});
-    $("#layer1").mousemove(function(e){handleMouseMove(e);});
-    $("#layer1").mouseup(function(e){handleMouseUp(e);});
-    $("#layer1").mouseout(function(e){handleMouseOut(e);});
+    $("#canvas").mousedown(function(e){handleMouseDown(e);});
+    $("#canvas").mousemove(function(e){handleMouseMove(e);});
+    $("#canvas").mouseup(function(e){handleMouseUp(e);});
+    $("#canvas").mouseout(function(e){handleMouseOut(e);});
 
 
 		var inputedText = $('#inputed').val();//抓取頁面inputed ID的內容
@@ -342,8 +333,6 @@ function photoClick(id) {
 	
 
         	ctx.clearRect(0,0,canvasWidth,canvasHeight); //移除canvas起始的內容
-        	ctx2.clearRect(0,0,canvasWidth,canvasHeight); //移除canvas起始的內容
-
 			selectIMG = document.getElementById(id);//抓html裡預載入的照片
 			selectIMG.crossOrigin = "Anonymous"; // 這務必要做，為了讓Facebook的照片能夠crossdomain傳入到你的頁面，CORS Policy請參考https://developer.mozilla.org/en-US/docs/Web/HTML/CORS_enabled_image 
 			// canvas.width = selectIMG.width;//設定canvas的大小需符合selectIMG的大小
@@ -356,7 +345,7 @@ function photoClick(id) {
 
 			ctx.drawImage(selectIMG,0,0);//從XY軸0，0值開始畫如selectIMG
 			ctx.drawImage(img3,canMouseX-128/2,canMouseY-120/2); //劃入img3，並根據你的滑鼠游標移動，你可以自行更換想要移動的圖層，數值會因XY軸向有所不同
-			ctx2.drawImage(img2,0,0); //劃入img2
+			ctx.drawImage(img2,0,0); //劃入img2
 			// var inputedText = $('#inputed').val();//抓取頁面inputed ID的內容
 			// ctx.fillStyle = "black"; //字體顏色
 			// ctx.font='20px "微軟正黑體"'; //字體大小和字形
@@ -430,7 +419,7 @@ function getMyUrl() {
 	img2.src = inputedText;
 	if (selectIMG != undefined) {
 	  	    ctx.clearRect(0,0,canvasWidth,canvasHeight); //移除canvas起始的內容
-			// ctx.drawImage(img2,0,0); //劃入img2
+			ctx.drawImage(img2,0,0); //劃入img2
 	  		ctx.drawImage(selectIMG,canMouseX-150,canMouseY); //劃入img3，並根據你的滑鼠游標移動，你可以自行更換想要移動的圖層，數值會因XY軸向有所不同
 	     }
 
@@ -440,7 +429,7 @@ function getMyUrl() {
 // 但思考authToken該怎麼拿到，因為這裡我並沒有把使用者登入的token載入到這函數內，所以它是不會得到token的
 function PostImageToFacebook(authToken) {
 	$('.info').append('<img src="img/loading.gif"/>')//載入loading的img
-    var canvas = document.getElementById("layer1");//找canvas
+    var canvas = document.getElementById("canvas");//找canvas
     var imageData = canvas.toDataURL("image/png");//把canvas轉換PNG
     try {
         blob = dataURItoBlob(imageData);//把影像載入轉換函數
