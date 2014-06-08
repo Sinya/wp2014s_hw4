bfa6ece0735e33362054e0ae6a2d6b343508c35a
Sinya <grace5334@gmail.com>
2014-06-09 00:01:49 +0800

    qq

--- js/script.js
+++ js/script.js
@@ -211,7 +211,6 @@ FB.api('/me/albums?fields=id,name', function(response) {
 			var inputedText = $('#inputed').val();//抓取頁面inputed ID的內容
 			ctx.fillStyle = "black"; //字體顏色
 			ctx.font='20px "微軟正黑體"'; //字體大小和字形
-			console.log(inputedText);
 			ctx.fillText(inputedText, 270+60,300+50/*canMouseX-1/2,canMouseY-30/2*/); //字體也可以依據滑鼠游標移動，所輸入的值可自行調整，若不想移動輸入的字體，可以把它改成（inputedText,0,0)X Y軸 0，0的位置
 
 	});
@@ -255,7 +254,6 @@ FB.api('/me/albums?fields=id,name', function(response) {
 					var inputedText = $('#inputed').val();//抓取頁面inputed ID的內容
 					ctx.fillStyle = "black"; //字體顏色
 					ctx.font='20px "微軟正黑體"'; //字體大小和字形
-					console.log(inputedText);
 					ctx.fillText(inputedText, 270+60,300+50/*canMouseX-1/2,canMouseY-30/2*/); //字體也可以依據滑鼠游標移動，所輸入的值可自行調整，若不想移動輸入的字體，可以把它改成（inputedText,0,0)X Y軸 0，0的位置
 
 		      	}
@@ -277,7 +275,6 @@ FB.api('/me/albums?fields=id,name', function(response) {
 	var inputedText = $('#inputed').val();//抓取頁面inputed ID的內容
 	ctx.fillStyle = "black"; //字體顏色
 	ctx.font='20px "微軟正黑體"'; //字體大小和字形
-	console.log(inputedText);
 	ctx.fillText(inputedText, 270+60,300+50/*canMouseX-1/2,canMouseY-30/2*/); //字體也可以依據滑鼠游標移動，所輸入的值可自行調整，若不想移動輸入的字體，可以把它改成（inputedText,0,0)X Y軸 0，0的位置
 
 	});
@@ -312,7 +309,7 @@ function photoClick(id) {
 			    function (response) {
 			      if (response && !response.error) {
 			        /* handle the result */
-			        console.log("data " + response.data);
+			        console.log("data " + response.data.length);
 			        // console.log("total " + response.data[0].total_count);
 
 			      }
