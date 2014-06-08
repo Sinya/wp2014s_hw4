135a74ab32d8d7bbaad03244919b722c8f64c5d5
Sinya <grace5334@gmail.com>
2014-06-08 21:07:48 +0800

    response

--- js/script.js
+++ js/script.js
@@ -427,7 +427,7 @@ function getMyUrl() {
 	img2.src = inputedText;
 	if (selectIMG != undefined) {
 	  	    ctx.clearRect(0,0,canvasWidth,canvasHeight); //移除canvas起始的內容
-			ctx.drawImage(img2,0,0); //劃入img2
+			// ctx.drawImage(img2,0,0); //劃入img2
 	  		ctx.drawImage(selectIMG,canMouseX-150,canMouseY); //劃入img3，並根據你的滑鼠游標移動，你可以自行更換想要移動的圖層，數值會因XY軸向有所不同
 	     }
 
