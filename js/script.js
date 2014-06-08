a6d0349dfe80c4a94d86014744370f2d9f86821a
Sinya <grace5334@gmail.com>
2014-06-08 23:25:19 +0800

    qq

--- js/script.js
+++ js/script.js
@@ -334,6 +334,7 @@ FB.api('/me/albums?fields=id,name', function(response) {
 
 	
 	$("#inputed").mouseover(function() {
+	ctx.drawImage(img3,270,300); //劃入img3，並根據你的滑鼠游標移動，你可以自行更換想要移動的圖層，數值會因XY軸向有所不同
 	var inputedText = $('#inputed').val();//抓取頁面inputed ID的內容
 	ctx.fillStyle = "black"; //字體顏色
 	ctx.font='20px "微軟正黑體"'; //字體大小和字形
