be930004ede108007b91201023424e026cfbd305
Sinya <grace5334@gmail.com>
2014-06-09 22:41:48 +0800

    add

--- js/script.js
+++ js/script.js
@@ -213,7 +213,7 @@ function photoClick(id) {
 			$("#photoContainer strong").remove();
         	ctx.clearRect(0,0,canvasWidth,canvasHeight); //移除canvas起始的內容
 			selectIMG = document.getElementById(id);//抓html裡預載入的照片
-			selectIMG.crossOrigin = "Anonymous"; // 這務必要做，為了讓Facebook的照片能夠crossdomain傳入到你的頁面，CORS Policy請參考https://developer.mozilla.org/en-US/docs/Web/HTML/CORS_enabled_image 
+			selectIMG.crossOrigin = "anonymous"; // 這務必要做，為了讓Facebook的照片能夠crossdomain傳入到你的頁面，CORS Policy請參考https://developer.mozilla.org/en-US/docs/Web/HTML/CORS_enabled_image 
 			// canvas.width = selectIMG.width;//設定canvas的大小需符合selectIMG的大小
 			// canvas.height = selectIMG.height;
 
