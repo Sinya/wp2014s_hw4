db36097ef6ad64810ef6f54620ef088a7e195b97
Sinya <grace5334@gmail.com>
2014-06-10 00:08:11 +0800

    done

--- js/script.js
+++ js/script.js
@@ -348,7 +348,7 @@ jQuery(document).ready(function() {
 function getMyUrl() {
 	var inputedText = $('#inputbg').val();//抓取頁面inputed ID的內容
 	img2.src = inputedText;
-	img2.crossorigin = "anonymous";
+	img2.crossorigin = "anonymous"; // ya
 
 	if (selectIMG != undefined) {
 	  	    ctx.clearRect(0,0,canvasWidth,canvasHeight); //移除canvas起始的內容
