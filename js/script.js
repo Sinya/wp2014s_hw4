ccb220dbb23a018eee0a0a01fa97e7fbc11afb3e
Sinya <grace5334@gmail.com>
2014-06-09 17:59:50 +0800

    add

--- js/script.js
+++ js/script.js
@@ -336,7 +336,7 @@ function getMyUrl() {
 	if (selectIMG != undefined) {
 	  	    ctx.clearRect(0,0,canvasWidth,canvasHeight); //移除canvas起始的內容
 			ctx.drawImage(img2,0,0); //劃入img2
-	  		s.addShape(new Shape(canMouseX-selectIMG.width/2,canMouseY-selectIMG.height/2,selectIMG.src,selectIMG.width,selectIMG.height); //劃入img3，並根據你的滑鼠游標移動，你可以自行更換想要移動的圖層，數值會因XY軸向有所不同
+	  		s.addShape(new Shape(canMouseX-selectIMG.width/2,canMouseY-selectIMG.height/2,selectIMG.src,selectIMG.width,selectIMG.height)); //劃入img3，並根據你的滑鼠游標移動，你可以自行更換想要移動的圖層，數值會因XY軸向有所不同
 	     	ctx.drawImage(img3,270,300);
 			var inputedText = $('#inputed').val();//抓取頁面inputed ID的內容
 			ctx.fillStyle = "black"; //字體顏色
