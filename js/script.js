a0b0c1e703ec475f26a3bf50cec477b85ac6efce
Sinya <grace5334@gmail.com>
2014-06-09 17:06:44 +0800

    add

--- js/script.js
+++ js/script.js
@@ -182,53 +182,53 @@ FB.api('/me/albums?fields=id,name', function(response) {
 
 	// $("#canvas").mouseover(function() {
 
-	function handleMouseDown(e){//滑鼠按下的函數
-      canMouseX=parseInt(e.clientX-offsetX);//抓滑鼠游標X
-      canMouseY=parseInt(e.clientY-offsetY);//抓滑鼠游標y
-      // set the drag flag
-      isDragging=true;//宣告拖拉變數
-    }
+	// function handleMouseDown(e){//滑鼠按下的函數
+ //      canMouseX=parseInt(e.clientX-offsetX);//抓滑鼠游標X
+ //      canMouseY=parseInt(e.clientY-offsetY);//抓滑鼠游標y
+ //      // set the drag flag
+ //      isDragging=true;//宣告拖拉變數
+ //    }
 
-    function handleMouseUp(e){//滑鼠放掉的函數
-      canMouseX=parseInt(e.clientX-offsetX);
-      canMouseY=parseInt(e.clientY-offsetY);
-      // clear the drag flag
-      isDragging=false;
-    }
+ //    function handleMouseUp(e){//滑鼠放掉的函數
+ //      canMouseX=parseInt(e.clientX-offsetX);
+ //      canMouseY=parseInt(e.clientY-offsetY);
+ //      // clear the drag flag
+ //      isDragging=false;
+ //    }
 
-    function handleMouseOut(e){//滑鼠移開的函數
-      canMouseX=parseInt(e.clientX-offsetX);
-      canMouseY=parseInt(e.clientY-offsetY);
-      // user has left the canvas, so clear the drag flag
-      isDragging=false;
-    }
+   //  function handleMouseOut(e){//滑鼠移開的函數
+   //    canMouseX=parseInt(e.clientX-offsetX);
+   //    canMouseY=parseInt(e.clientY-offsetY);
+   //    // user has left the canvas, so clear the drag flag
+   //    isDragging=false;
+   //  }
 
-    function handleMouseMove(e){//滑鼠移動的event
-      canMouseX=parseInt(e.clientX-offsetX);
-      canMouseY=parseInt(e.clientY-offsetY);
-   // if the drag flag is set, clear the canvas and draw the image
-	      if(isDragging){ //當拖拉為True時
+   //  function handleMouseMove(e){//滑鼠移動的event
+   //    canMouseX=parseInt(e.clientX-offsetX);
+   //    canMouseY=parseInt(e.clientY-offsetY);
+   // // if the drag flag is set, clear the canvas and draw the image
+	  //     if(isDragging){ //當拖拉為True時
 	  
-			  if (selectIMG != undefined) {
-			  	    ctx.clearRect(0,0,canvasWidth,canvasHeight); //移除canvas起始的內容
-	  				ctx.drawImage(img2,0,0); //劃入img2
-					ctx.drawImage(selectIMG,canMouseX-selectIMG.width/2,canMouseY-selectIMG.height/2,selectIMG.width,selectIMG.height);//從XY軸0，0值開始畫如profileimg
-		      		// ctx.drawImage(selectIMG,canMouseX-150,canMouseY); //劃入img3，並根據你的滑鼠游標移動，你可以自行更換想要移動的圖層，數值會因XY軸向有所不同
-				    ctx.drawImage(img3,270,300);
-					var inputedText = $('#inputed').val();//抓取頁面inputed ID的內容
-					ctx.fillStyle = "black"; //字體顏色
-					ctx.font='20px "微軟正黑體"'; //字體大小和字形
-					ctx.fillText(inputedText, 270+60,300+50/*canMouseX-1/2,canMouseY-30/2*/); //字體也可以依據滑鼠游標移動，所輸入的值可自行調整，若不想移動輸入的字體，可以把它改成（inputedText,0,0)X Y軸 0，0的位置
+			//   if (selectIMG != undefined) {
+			//   	    ctx.clearRect(0,0,canvasWidth,canvasHeight); //移除canvas起始的內容
+	  // 				ctx.drawImage(img2,0,0); //劃入img2
+			// 		ctx.drawImage(selectIMG,canMouseX-selectIMG.width/2,canMouseY-selectIMG.height/2,selectIMG.width,selectIMG.height);//從XY軸0，0值開始畫如profileimg
+		 //      		// ctx.drawImage(selectIMG,canMouseX-150,canMouseY); //劃入img3，並根據你的滑鼠游標移動，你可以自行更換想要移動的圖層，數值會因XY軸向有所不同
+			// 	    ctx.drawImage(img3,270,300);
+			// 		var inputedText = $('#inputed').val();//抓取頁面inputed ID的內容
+			// 		ctx.fillStyle = "black"; //字體顏色
+			// 		ctx.font='20px "微軟正黑體"'; //字體大小和字形
+			// 		ctx.fillText(inputedText, 270+60,300+50/*canMouseX-1/2,canMouseY-30/2*/); //字體也可以依據滑鼠游標移動，所輸入的值可自行調整，若不想移動輸入的字體，可以把它改成（inputedText,0,0)X Y軸 0，0的位置
 
-		      	}
-	     }
-     }
+		 //      	}
+	  //    }
+   //   }
 
 	// 抓取滑鼠移動的event
-    $("#canvas").mousedown(function(e){handleMouseDown(e);});
-    $("#canvas").mousemove(function(e){handleMouseMove(e);});
-    $("#canvas").mouseup(function(e){handleMouseUp(e);});
-    $("#canvas").mouseout(function(e){handleMouseOut(e);});
+    // $("#canvas").mousedown(function(e){handleMouseDown(e);});
+    // $("#canvas").mousemove(function(e){handleMouseMove(e);});
+    // $("#canvas").mouseup(function(e){handleMouseUp(e);});
+    // $("#canvas").mouseout(function(e){handleMouseOut(e);});
 
 	$("#inputed").mouseover(function() {
 	ctx.drawImage(img3,270,300);
